f54f7380c75a897c0bd9a1bf983ef00bb92b6dba
Peter Job <ptjob@icloud.com>
2024-12-29 18:28:41 +1100

    chore: refactor setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-#!/bin/zsh
+#!/usr/bin/env bash
+
+# Setup script for my dotfiles
+# Todo: I don't like using branches to manage different OSes. If they never converge, seems like a bad idea.
+
+#region Check OS and branch (WIP)
 
 # Extract the OS information
 os=$(uname -s)
@@ -19,6 +24,9 @@ else
     exit 1
 fi
 
+#endregion
+
+#region Install dependencies
 
 # Install omzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -26,8 +34,13 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+#endregion
+
+#region Sync dotfiles
+
 # Copy dotfiles
 cp ./.* ~/
 
-echo "Setup complete"
+#endregion
 
+echo "Setup complete"
