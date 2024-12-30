e01e242373e0439f7e6cfbb51c0926ad5e92adf6
Peter Job <ptjob@icloud.com>
2024-12-30 23:05:24 +1100

    feat: working chezmoi setup

---
+++ install.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# Install dotfiles via chezmoi
+# Copied from [felipecrs' dotfiles install script](https://github.com/felipecrs/dotfiles/blob/c3e0e400e544b3918a856cb10b55672c6ab4c827/install.sh)
+# See also [twpayne's original install script](https://github.com/twpayne/dotfiles/blob/cb5ba7a0ba192c7c4a99e411980a4da9848558d7/install.sh)
+
+# -e: exit on error
+# -u: exit on unset variables
+set -eu
+
+# todo: refactor, too many functions
+log_color() {
+  color_code="$1"
+  shift
+
+  printf "\033[${color_code}m%s\033[0m\n" "$*" >&2
+}
+
+log_red() {
+  log_color "0;31" "$@"
+}
+
+log_blue() {
+  log_color "0;34" "$@"
+}
+
+log_task() {
+  log_blue "🔃" "$@"
+}
+
+log_error() {
+  log_red "❌" "$@"
+}
+
+error() {
+  log_error "$@"
+  exit 1
+}
+
+if ! chezmoi="$(command -v chezmoi)"; then
+  bin_dir="${HOME}/.local/bin"
+  chezmoi="${bin_dir}/chezmoi"
+  log_task "Installing chezmoi to '${chezmoi}'"
+  if command -v curl >/dev/null; then
+    chezmoi_install_script="$(curl -fsSL https://get.chezmoi.io)"
+  elif command -v wget >/dev/null; then
+    chezmoi_install_script="$(wget -qO- https://get.chezmoi.io)"
+  else
+    error "To install chezmoi, you must have curl or wget."
+  fi
+  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
+  unset chezmoi_install_script bin_dir
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+# shellcheck disable=SC2312
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+set -- init --source="${script_dir}" --verbose=false "$@"
+
+if [ -n "${DOTFILES_ONE_SHOT-}" ]; then
+  set -- "$@" --one-shot
+else
+  set -- "$@" --apply
+fi
+
+if [ -n "${DOTFILES_DEBUG-}" ]; then
+  set -- "$@" --debug
+fi
+
+log_task "Running 'chezmoi $*'"
+# replace current process with chezmoi
+exec "${chezmoi}" "$@"
--- readme.md
+++ readme.md
@@ -4,9 +4,11 @@
 
 ```bash
 git clone <repo>
-./setup.sh
+./install.sh
 ```
 
+<!-- Todo: merge in layouts from avb dotfiles and manage with chezmoi-->
+<!--
 ### MacOS keyboard layout
 
 1. Copy the keyboard layout files to the `Keyboard Layouts` directory in your home directory.
@@ -18,3 +20,4 @@ git clone <repo>
 2. Go to `System Settings` > `Keyboard` > `Text Input` > `Input Sources` > `Edit`.
 3. Click `+` in the bottom left, scroll to the bottom of the list on the left and select `Others` then select `QWERTY no option` and click `Add`.
 4. Either delete your existing layout (probably called `U.S.`) or just switch to the `QWERTY no option` layout using the input-source select in the menu bar.
+-->
