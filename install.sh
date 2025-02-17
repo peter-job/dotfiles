a0e7de23a2233158ecd6f8fd6bb444480937466f
Peter Job <ptjob@icloud.com>
2025-02-17 13:47:04 +1100

    chore: clean up install script

--- install.sh
+++ install.sh
@@ -1,78 +1,36 @@
 #!/bin/sh
-#
+
 # Install dotfiles via chezmoi
-# Copied from [felipecrs' dotfiles install script](https://github.com/felipecrs/dotfiles/blob/c3e0e400e544b3918a856cb10b55672c6ab4c827/install.sh)
-# See also [twpayne's original install script](https://github.com/twpayne/dotfiles/blob/cb5ba7a0ba192c7c4a99e411980a4da9848558d7/install.sh)
 
-# -e: exit on error
-# -u: exit on unset variables
-set -eu
+set -e # -e: exit on error
 
-# todo: refactor, too many functions
-log_color() {
-  color_code="$1"
-  shift
-
-  printf "\033[${color_code}m%s\033[0m\n" "$*" >&2
-}
-
-log_red() {
-  log_color "0;31" "$@"
-}
-
-log_blue() {
-  log_color "0;34" "$@"
-}
-
-log_task() {
-  log_blue "🔃" "$@"
-}
-
-log_error() {
-  log_red "❌" "$@"
-}
-
-error() {
-  log_error "$@"
-  exit 1
-}
-
-if ! chezmoi="$(command -v chezmoi)"; then
-  bin_dir="${HOME}/.local/bin"
-  chezmoi="${bin_dir}/chezmoi"
-  log_task "Installing chezmoi to '${chezmoi}'"
-  if command -v curl >/dev/null; then
-    chezmoi_install_script="$(curl -fsSL https://get.chezmoi.io)"
-  elif command -v wget >/dev/null; then
-    chezmoi_install_script="$(wget -qO- https://get.chezmoi.io)"
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
   else
-    error "To install chezmoi, you must have curl or wget."
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
   fi
-  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-  unset chezmoi_install_script bin_dir
+else
+  chezmoi=chezmoi
 fi
 
-set -- init --verbose=false "$@"
+args="init --apply"
 
 if [ -n "${DOTFILES_USE_LOCAL_SOURCE-}" ]; then
   # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
   # shellcheck disable=SC2312
   script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-  set -- "$@" --source="${script_dir}"
+  args="$args --source=\"$script_dir\""
 else
-  set -- "$@" "${DOTFILES_GIT_USERNAME:-peter-job}"
+  args="$args ${DOTFILES_GITHUB_USERNAME:-peter-job}"
 fi
 
-if [ -n "${DOTFILES_ONE_SHOT-}" ]; then
-  set -- "$@" --one-shot
-else
-  set -- "$@" --apply
-fi
+echo "Executing '$chezmoi $args'"
 
-if [ -n "${DOTFILES_DEBUG-}" ]; then
-  set -- "$@" --debug
-fi
-
-log_task "Running 'chezmoi $*'"
-# replace current process with chezmoi
-exec "${chezmoi}" "$@"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" "$args"
